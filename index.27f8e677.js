cb2245676a8340ab7d8714e7a6adc4a6650aa886
Budankov <107200706+Budankov@users.noreply.github.com>
2022-11-30 17:27:45 +0000

    Deploying to gh-pages from @ Budankov/goit-js-hw-10@33ceddadd16ee4c78df322bff81adcdc97795737 🚀

--- index.27f8e677.js
+++
@@ -1,2 +0,0 @@
-fetch(`https://restcountries.com/v3.1/name/${"peru"}?fields=name,capital,population,flags,languages`).then((e=>e.json())).then((e=>{console.log(e)})).catch((e=>{console.log(e)}));
-//# sourceMappingURL=index.27f8e677.js.map
--- index.69f4bbf3.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/".concat("peru","?fields=name,capital,population,flags,languages")).then((function(n){return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));
-//# sourceMappingURL=index.69f4bbf3.js.map
