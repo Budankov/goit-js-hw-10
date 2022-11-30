7ba0cc1078aaca5735bc16a2845b7d7f39eb8299
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2022-11-30 20:14:12 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import fetchCountries, { fetchCountries } from './js/fetchCountries';
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.querySelector('#search-box');
-console.log(searchBox);
+const countryInfo = document.querySelector('.country-info');
 
-fetchCountries('peru');
+searchBox.addEventListener('input', onInputChange);
+
+function onInputChange(e) {
+  console.log(e.currentTarget.value);
+}
 
 const createMarkupCardCountry = ({
   name: { official },
@@ -19,16 +23,37 @@ const createMarkupCardCountry = ({
      <div class="card-header">
         <img
         class="card-flags-img"
-        src="https://flagcdn.com/pe.svg"
+        src="${svg}"
         alt="Flags"
         width="50"
         />
-        <h1 class="card-title">Republic of Peru</h1>
+        <h1 class="card-title">${official}</h1>
     </div>
-    <p class="card-subtitle"><span>Capital: </span>Lima</p>
-    <p class="card-subtitle"><span>Population: </span>329912312</p>
-    <p class="card-subtitle"><span>Languages: </span>Spanish</p>
+    <p class="card-subtitle"><span>Capital: </span>${capital}</p>
+    <p class="card-subtitle"><span>Population: </span>${population}</p>
+    <p class="card-subtitle"><span>Languages: </span>${languages}</p>
     </div>`;
-// const makeGalleryItem = galleryItems.map(createMarkupElemetsGallery).join('');
+// const makeCardCountry = fetchCountries.map(createMarkupCardCountry).join('');
 
-// galleryListEl.insertAdjacentHTML('beforeend', makeGalleryItem);
+// countryInfo.insertAdjacentHTML('beforeend', makeCardCountry);
+
+function renderCardCountry(country) {
+  const markup = createMarkupCardCountry(country);
+  console.log(markup);
+}
+
+function fetchCountries(name) {
+  fetch(
+    // `https://restcountries.com/v3.1/name/peru?fields=name,capital,population,flags,languages`
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(renderCardCountry)
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+fetchCountries('peru');
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,13 @@
-export function fetchCountries(name) {
-  fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  )
-    .then(response => {
-      return response.json();
-    })
-    .then(country => {
-      console.log(country);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
+// export function fetchCountries(name) {
+//   fetch(
+//     `https://restcountries.com/v3.1/name/peru?fields=name,capital,population,flags,languages`
+//     // `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+//   )
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(renderCardCountry)
+//     .catch(error => {
+//       console.log(error);
+//     });
+// }
