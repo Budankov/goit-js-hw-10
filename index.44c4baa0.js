af7666b27ab7aa62382184e7de5d4ec86b236f47
Budankov <107200706+Budankov@users.noreply.github.com>
2022-11-30 18:31:12 +0000

    Deploying to gh-pages from @ Budankov/goit-js-hw-10@8220e73a69613685207caa4a34079ffd922e2929 🚀

--- index.44c4baa0.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#search-box");document.querySelector(".country-info");var n;e.addEventListener("input",(function(e){console.log(e.currentTarget.value)})),fetch(`https://restcountries.com/v3.1/name/${n}?fields=name,capital,population,flags,languages`).then((e=>e.json())).then(renderCardCountry).catch((e=>{console.log(e)}));
-//# sourceMappingURL=index.44c4baa0.js.map
