60526c6a2547f27ce23fcd0b4c3143ee098283e7
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2022-12-01 22:41:14 +0200

    Update

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -16,7 +16,6 @@ export function fetchCountries(name) {
   )
     .then(response => {
       if (!response.ok) {
-        // Notiflix.Notify.failure('Oops, there is no country with that name');
         throw new Error(response.status);
       }
       return response.json();
